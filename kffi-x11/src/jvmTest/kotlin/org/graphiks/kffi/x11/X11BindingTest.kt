6cc51b178c23d93bc0c70db8a97468a04217fad3
ygdrasil-io <alexandre.mommers@gmail.com>
2026-08-22 16:21:05 +0200

    test: specify generated x11 struct behavior

--- kffi-x11/src/jvmTest/kotlin/org/graphiks/kffi/x11/X11BindingTest.kt
+++ kffi-x11/src/jvmTest/kotlin/org/graphiks/kffi/x11/X11BindingTest.kt
@@ -10,16 +10,36 @@ import kotlin.test.assertEquals
 import kotlin.test.assertIs
 import kotlin.test.assertTrue
 import org.graphiks.kffi.x11.generated.KffiXEventStorage
+import org.graphiks.kffi.x11.generated.AllPlanes
 import org.graphiks.kffi.x11.generated.KeyPress
+import org.graphiks.kffi.x11.generated.IsViewable
 import org.graphiks.kffi.x11.generated.XCloseDisplay
 import org.graphiks.kffi.x11.generated.XArc
+import org.graphiks.kffi.x11.generated.XClientMessageEvent
 import org.graphiks.kffi.x11.generated.XColor
+import org.graphiks.kffi.x11.generated.XImage
 import org.graphiks.kffi.x11.generated.XOpenDisplay
 import org.graphiks.kffi.x11.generated.XPoint
 import org.graphiks.kffi.x11.generated.XRectangle
+import org.graphiks.kffi.x11.generated.XSelectionEvent
 import org.graphiks.kffi.x11.generated.XSegment
 import org.graphiks.kffi.x11.generated.XShmSegmentInfoCompat
+import org.graphiks.kffi.x11.generated.XSetWindowAttributes
 import org.graphiks.kffi.x11.generated.XSetForeground
+import org.graphiks.kffi.x11.generated.XSHM_ZPIXMAP
+import org.graphiks.kffi.x11.generated.XWindowAttributes
+import org.graphiks.kffi.x11.generated.ZPixmap
+import org.graphiks.kffi.x11.generated.XNClientWindow
+import org.graphiks.kffi.x11.generated.XNCommitStringCallback
+import org.graphiks.kffi.x11.generated.XNFocusWindow
+import org.graphiks.kffi.x11.generated.XNInputStyle
+import org.graphiks.kffi.x11.generated.XNPreeditAttributes
+import org.graphiks.kffi.x11.generated.XNPreeditDoneCallback
+import org.graphiks.kffi.x11.generated.XNPreeditDrawCallback
+import org.graphiks.kffi.x11.generated.XNPreeditStartCallback
+import org.graphiks.kffi.x11.generated.XNArea
+import org.graphiks.kffi.x11.generated.XNAreaNeeded
+import org.graphiks.kffi.x11.generated.XNSpotLocation
 
 class X11BindingTest {
     @Test
@@ -49,10 +69,28 @@ class X11BindingTest {
                 "XShmCreateImage",
                 "XCompositeNameWindowPixmap",
                 "XDestroyImage",
+                "XQueryBestCursor",
+                "XCreateIC",
+                "XSetICValues",
                 "KeyRelease",
             ).all { expected -> declarations.any { it.name == expected } },
         )
         assertEquals(2, KeyPress())
+        assertEquals(2, ZPixmap())
+        assertEquals(-1L, AllPlanes())
+        assertEquals(2, XSHM_ZPIXMAP())
+        assertEquals(2, IsViewable())
+        assertEquals("inputStyle", XNInputStyle)
+        assertEquals("clientWindow", XNClientWindow)
+        assertEquals("focusWindow", XNFocusWindow)
+        assertEquals("preeditAttributes", XNPreeditAttributes)
+        assertEquals("area", XNArea)
+        assertEquals("areaNeeded", XNAreaNeeded)
+        assertEquals("spotLocation", XNSpotLocation)
+        assertEquals("preeditStartCallback", XNPreeditStartCallback)
+        assertEquals("preeditDrawCallback", XNPreeditDrawCallback)
+        assertEquals("preeditDoneCallback", XNPreeditDoneCallback)
+        assertEquals("commitStringCallback", XNCommitStringCallback)
 
         val xOpenDisplay = declarations.single { it.name == "XOpenDisplay" }
         assertEquals(MemorySegment::class.java, xOpenDisplay.returnType)
@@ -68,6 +106,32 @@ class X11BindingTest {
         val xDestroyImage = declarations.single { it.name == "XDestroyImage" }
         assertEquals(Int::class.javaPrimitiveType, xDestroyImage.returnType)
         assertContentEquals(arrayOf(MemorySegment::class.java), xDestroyImage.parameterTypes)
+
+        val xQueryBestCursor = declarations.single { it.name == "XQueryBestCursor" }
+        assertEquals(Int::class.javaPrimitiveType, xQueryBestCursor.returnType)
+        assertContentEquals(
+            arrayOf(
+                MemorySegment::class.java,
+                Long::class.javaPrimitiveType,
+                Int::class.javaPrimitiveType,
+                Int::class.javaPrimitiveType,
+                MemorySegment::class.java,
+                MemorySegment::class.java,
+            ),
+            xQueryBestCursor.parameterTypes,
+        )
+
+        val xSetICValues = declarations.single { it.name == "XSetICValues" }
+        assertEquals(MemorySegment::class.java, xSetICValues.returnType)
+        assertContentEquals(
+            arrayOf(
+                MemorySegment::class.java,
+                MemorySegment::class.java,
+                MemorySegment::class.java,
+                MemorySegment::class.java,
+            ),
+            xSetICValues.parameterTypes,
+        )
     }
 
     @Test
@@ -96,6 +160,73 @@ class X11BindingTest {
         assertEquals(16L, XColor.layout.byteSize())
         assertEquals(8L, XSegment.layout.byteSize())
         assertEquals(12L, XArc.layout.byteSize())
+    }
+
+    @Test
+    fun generatedRecordAccessorsRoundTripNativeFields() {
+        val arena = java.lang.foreign.Arena.ofConfined()
+
+        arena.use {
+            val image = XImage.allocate(it)
+            val pixels = it.allocate(16)
+            XImage.data(image, pixels)
+            XImage.bytes_per_line(image, 64)
+            XImage.bits_per_pixel(image, 32)
+            assertEquals(pixels, XImage.data(image))
+            assertEquals(64, XImage.bytes_per_line(image))
+            assertEquals(32, XImage.bits_per_pixel(image))
+
+            val attributes = XWindowAttributes.allocate(it)
+            XWindowAttributes.map_state(attributes, IsViewable())
+            assertEquals(IsViewable(), XWindowAttributes.map_state(attributes))
+
+            val clientMessage = XClientMessageEvent.allocate(it)
+            XClientMessageEvent.type(clientMessage, 33)
+            XClientMessageEvent.window(clientMessage, 0x10203040L)
+            XClientMessageEvent.message_type(clientMessage, 0x50607080L)
+            XClientMessageEvent.format(clientMessage, 32)
+            XClientMessageEvent.data_l0(clientMessage, 0x1122334455667788L)
+            assertEquals(33, XClientMessageEvent.type(clientMessage))
+            assertEquals(0x10203040L, XClientMessageEvent.window(clientMessage))
+            assertEquals(0x50607080L, XClientMessageEvent.message_type(clientMessage))
+            assertEquals(32, XClientMessageEvent.format(clientMessage))
+            assertEquals(0x1122334455667788L, XClientMessageEvent.data_l0(clientMessage))
+
+            val selection = XSelectionEvent.allocate(it)
+            XSelectionEvent.requestor(selection, 0x1111L)
+            XSelectionEvent.selection(selection, 0x2222L)
+            XSelectionEvent.target(selection, 0x3333L)
+            XSelectionEvent.property(selection, 0x4444L)
+            assertEquals(0x1111L, XSelectionEvent.requestor(selection))
+            assertEquals(0x2222L, XSelectionEvent.selection(selection))
+            assertEquals(0x3333L, XSelectionEvent.target(selection))
+            assertEquals(0x4444L, XSelectionEvent.property(selection))
+
+            val windowAttributes = XSetWindowAttributes.allocate(it)
+            XSetWindowAttributes.override_redirect(windowAttributes, 1)
+            assertEquals(1, XSetWindowAttributes.override_redirect(windowAttributes))
+        }
+    }
+
+    @Test
+    fun generatedRecordLayoutsMatchLinuxX11Abi() {
+        assertEquals(136L, XImage.layout.byteSize())
+        assertEquals(16L, XImage.layout.byteOffset(groupElement("data")))
+        assertEquals(44L, XImage.layout.byteOffset(groupElement("bytes_per_line")))
+        assertEquals(48L, XImage.layout.byteOffset(groupElement("bits_per_pixel")))
+
+        assertEquals(160L, XWindowAttributes.layout.byteSize())
+        assertEquals(116L, XWindowAttributes.layout.byteOffset(groupElement("map_state")))
+
+        assertEquals(96L, XClientMessageEvent.layout.byteSize())
+        assertEquals(16L, XClientMessageEvent.layout.byteOffset(groupElement("send_event")))
+        assertEquals(56L, XClientMessageEvent.layout.byteOffset(groupElement("data_l0")))
+
+        assertEquals(72L, XSelectionEvent.layout.byteSize())
+        assertEquals(32L, XSelectionEvent.layout.byteOffset(groupElement("requestor")))
+
+        assertEquals(112L, XSetWindowAttributes.layout.byteSize())
+        assertEquals(88L, XSetWindowAttributes.layout.byteOffset(groupElement("override_redirect")))
     }
 
     @Test
