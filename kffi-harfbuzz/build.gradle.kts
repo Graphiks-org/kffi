5d4df0ce40952f967986e4652c82ce352d117f51
ygdrasil-io <alexandre.mommers@gmail.com>
2026-09-19 14:02:24 +0200

    build(harfbuzz): skip module tests on unsupported platforms

--- kffi-harfbuzz/build.gradle.kts
+++ kffi-harfbuzz/build.gradle.kts
@@ -24,4 +24,8 @@ kotlin {
 tasks.withType<Test>().configureEach {
     useJUnitPlatform()
     jvmArgs("--enable-native-access=ALL-UNNAMED")
+    onlyIf("HarfBuzz supports macOS and Linux") {
+        System.getProperty("os.name").startsWith("Mac") ||
+            System.getProperty("os.name").startsWith("Linux")
+    }
 }
